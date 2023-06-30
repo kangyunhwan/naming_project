c4365c458316263bfe96294a9f2ca637ee5fcb1f
rkddbsghks <rkddbsghks12@gmail.com>
2023-06-30 21:03:20 +0900

    1

--- sub/portfolio/js/portfolio.js
+++ sub/portfolio/js/portfolio.js
@@ -61,47 +61,53 @@ document.addEventListener('DOMContentLoaded',()=>{
 
   }
 
-  const portfolioListWrap=document.querySelector("#portfolio_category_thumbnail_list")
-  const portfolioTabMenu=document.querySelectorAll("#portfolio_category_title_list>li");
 
-  let selectedTabMenu=portfolioTabMenu[0]
+  portfolioTabMenuWork();
 
-
-  for(item of portfolioTabMenu){
-    item.addEventListener('click',clickPortfolioTabMenu)
-  }
-
-  showPortfolio(1)
-
-  function clickPortfolioTabMenu(){
-    let clickIndex=getIndex(this);
-    activatePortfolioTabMenu(clickIndex);
-    showPortfolio(clickIndex+1)
-  }
+  function portfolioTabMenuWork(){
+    const portfolioListWrap=document.querySelector("#portfolio_category_thumbnail_list")
+    const portfolioTabMenu=document.querySelectorAll("#portfolio_category_title_list>li");
   
-
-  function showPortfolio(num){
-    axios.get('/sub/portfolio/portfolio_data'+num+'.html').then((response)=>{
-      portfolioListWrap.innerHTML=response.data
-    })
-  }
+    let selectedTabMenu=portfolioTabMenu[0]
   
-  function activatePortfolioTabMenu(index){
-    if(selectedTabMenu!=null && selectedTabMenu!=portfolioTabMenu[index]){
-      selectedTabMenu.classList.remove('selected');
-      
-
-      selectedTabMenu=portfolioTabMenu[index];
-      selectedTabMenu.classList.add('selected');
+  
+    for(item of portfolioTabMenu){
+      item.addEventListener('click',clickPortfolioTabMenu)
+    }
+  
+    showPortfolio(1)
+  
+    function clickPortfolioTabMenu(){
+      let clickIndex=getIndex(this);
+      activatePortfolioTabMenu(clickIndex);
+      showPortfolio(clickIndex+1)
+    }
+    
+  
+    function showPortfolio(num){
+      axios.get('/sub/portfolio/portfolio_data'+num+'.html').then((response)=>{
+        portfolioListWrap.innerHTML=response.data
+      })
+    }
+    
+    function activatePortfolioTabMenu(index){
+      if(selectedTabMenu!=null && selectedTabMenu!=portfolioTabMenu[index]){
+        selectedTabMenu.classList.remove('selected');
+        
+  
+        selectedTabMenu=portfolioTabMenu[index];
+        selectedTabMenu.classList.add('selected');
+      }
+    }
+  
+    function getIndex(menu){
+      let selectedIndex=0;
+      while((menu=menu.previousElementSibling)!=null){
+        selectedIndex++;
+      }
+      return selectedIndex;
     }
   }
-
-  function getIndex(menu){
-    let selectedIndex=0;
-    while((menu=menu.previousElementSibling)!=null){
-      selectedIndex++;
-    }
-    return selectedIndex;
-  }
+  
 
 })
