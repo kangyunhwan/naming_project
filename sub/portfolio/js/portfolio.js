ab0603b66ba2f2a7922188e6c14052905e54bbe8
kim sat byeol <ksbyeol93@gmail.com>
2023-06-30 18:10:44 +0900

    byeol_aticateDot_script

--- sub/portfolio/js/portfolio.js
+++ sub/portfolio/js/portfolio.js
@@ -1,6 +1,58 @@
 document.addEventListener('DOMContentLoaded',()=>{
+  
+  const portfolioSildeWrap=document.querySelector('#portfolio_silde_wrap')
+  const portfolioSildeList=document.querySelector('#portfolio_silde_list')
+  const portfolioSildeLi=document.querySelectorAll('#portfolio_silde_list>li')
+  const portfolioDot=document.querySelectorAll('#portfolio_silde_btn_list>li')
+  
+  let portfolioSildHeight=portfolioSildeList.offsetHeight;
+  let portfolioLength=portfolioSildeLi.length;
 
-  const
+  // let selectedDot=portfolioSildeDotBtn[0];
+  let currentIndex=0;
+  let selectedDot=portfolioDot[0]
+  let nextIndex=1;
+  
+  initEvent()
+
+  function initEvent(){
+    for(item of portfolioDot){
+      item.addEventListener('mouseenter',overDot)
+    }
+  }
+
+  function overDot(){
+    nextIndex=getIndex(this)
+    activateDot(nextIndex);
+    // alert(nextIndex)
+    
+    if(nextIndex>currentIndex){
+      slideNextImg(nextIndex)
+    }else if(nextIndex<currentIndex){
+      slidePrevImg(nextIndex)
+    }
+  }
+
+  function getIndex(checkMenu){
+    let selectedIndex=0;
+    while((checkMenu=checkMenu.previousElementSibling)!=null){
+      selectedIndex++;
+    }
+    return selectedIndex;
+  }
+
+  function activateDot(index){
+    if(selectedDot!=null && selectedDot!=portfolioDot[index]){
+      selectedDot.classList.remove('selected')
+    }
+
+    if(selectedDot!=portfolioDot[index]){
+      selectedDot=portfolioDot[index];
+      selectedDot.classList.add('selected')
+    }
+  }
+
+
 
 
 
