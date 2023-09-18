06ba28a43bcb759dbbf076895afef5a27f625ba8
rkddbsghks <rkddbsghks12@gmail.com>
2023-09-18 14:10:04 +0900

    20230918_1410

--- js/main_copy.js
+++ js/main_copy.js
@@ -7,30 +7,30 @@ document.addEventListener("DOMContentLoaded",()=>{
   
   gsap.set(copyWrap,{height:window.innerHeight})
 
-  // gsap.to(copyHi,{opacity:1,left:200,duration:1,scrollTrigger:{
-  //   trigger:copyHi,
-  //   // markers:true,
-  //   start:'0% 50%',
-  //   end:'bottom 0%',
-  //   toggleActions:'play reverse play reverse',
-  // }})
-  // gsap.to(copyTitle,{opacity:1,right:100,duration:1,scrollTrigger:{
-  //     trigger:copyTitle,
-  //     // markers:true,
-  //     start:'0% 50%',
-  //     end:'bottom 0%',
-  //     toggleActions:'play reverse play reverse',
-  //     toggleClass:'activate'
-  //   }})
+  gsap.to(copyHi,{opacity:1,left:200,duration:1,scrollTrigger:{
+    trigger:copyHi,
+    // markers:true,
+    start:'0% 50%',
+    end:'bottom 0%',
+    toggleActions:'play reverse play reverse',
+  }})
+  gsap.to(copyTitle,{opacity:1,right:100,duration:1,scrollTrigger:{
+      trigger:copyTitle,
+      // markers:true,
+      start:'0% 50%',
+      end:'bottom 0%',
+      toggleActions:'play reverse play reverse',
+      toggleClass:'activate'
+    }})
 
-  // gsap.to(copyArrow,{scrollTrigger:{
-  //   trigger:copyArrow,
-  //   // markers:true,
-  //   start:'-154% 50%',
-  //   end:'top top',
-  //   toggleClass:'arrow_activate'
+  gsap.to(copyArrow,{scrollTrigger:{
+    trigger:copyArrow,
+    // markers:true,
+    start:'-154% 50%',
+    end:'top top',
+    toggleClass:'arrow_activate'
     
     
-  // }})
+  }})
 
 })
